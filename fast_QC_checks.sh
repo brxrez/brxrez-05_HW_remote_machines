c52121f34c970b9714286bf0e81116f486e9e2cf
brxrez <brezende@dons.usfca.edu>
2018-09-21 23:55:54 +0000

    Update script

---
+++ fast_QC_checks.sh
@@ -0,0 +1,22 @@
+# !/bin/bash 
+# A script that condenses information about fasta files 
+# and produces output. 
+
+# Brian Rezende
+# brezende@dons.usfca.edu
+# September 21, 2018
+
+# Output from preliminary quality control checks on selected fasta files 
+echo Output for each fasta includes:
+echo Filename, file size, first three lines, last three lines,
+echo the number of sequences in the file, and the
+echo sequence identifier lines sorted alphabetically.
+echo --------------------------------------------------------------------
+
+# echo that this is a log file complied from fasta files
+echo Generating log file from fasta files 
+
+# echo the date and time of access 
+echo TZ='America/Los_Angeles' date
+
+
