6106a2b9f0e19e720852631edccfdfa96a0c2b9f
brxrez <brezende@dons.usfca.edu>
2018-09-25 05:52:43 +0000

    Create beginning information for script that includes expected output. Partially write the script with for loop.

--- fast_QC_checks.sh
+++ fast_QC_checks.sh
@@ -6,17 +6,40 @@
 # brezende@dons.usfca.edu
 # September 21, 2018
 
-# Output from preliminary quality control checks on selected fasta files 
-echo Output for each fasta includes:
-echo Filename, file size, first three lines, last three lines,
-echo the number of sequences in the file, and the
-echo sequence identifier lines sorted alphabetically.
-echo --------------------------------------------------------------------
-
 # echo that this is a log file complied from fasta files
-echo Generating log file from fasta files 
+echo Generating log file from fasta files
 
+# Display message indicating what will be presented
+echo Output from preliminary quality control checks on selected fasta files 
+
+echo
 # echo the date and time of access 
-echo TZ='America/Los_Angeles' date
+date +"%y-%m-%d %T"
 
+echo  
 
+# Detailing what output will include for each fasta file
+echo Output for each fasta includes:
+echo Filename, file size, first three lines, last three lines, 
+echo the number of sequences in the file, and the
+echo sequence identifier lines sorted alphabetically. 
+
+echo 
+echo "#############################################################################"
+
+echo 
+
+# Generating for loop for fasta file sequences
+for file in "$@"
+do
+	FILENAME="$file"
+	FILE_SIZE=$(du -h "$file" | cut -f 1)
+	SEQUENCE_LIST=$(bioawk -c fastx '(length($seq) > 1) {print ">"$name}' "$file" | sort) 
+	SEQUENCE_COUNT=$(bioawk -c fastx '(length($seq) > 1) {print ">"$name}' "$file" | wc -l)
+	echo Output for "$FILENAME":
+	echo Size: "$FILE_SIZE"
+	echo First three lines: 
+	echo -------------------
+	head -3 $file
+	tail -3 $file
+done 
